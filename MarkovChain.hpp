7ed198dfb0e63214ec9c6f1ce23e7303459c3f67
DiffeoMan <35209763+DiffeoMan@users.noreply.github.com>
2019-02-17 08:48:53 -0700

    v.0.1.0
    Contains class to model a Markov Chain as an object, driver to run the following Markov chain tasks: compute the stationary distribution(s), limiting matrix,  limiting distribution, number of paths between states, reachable states, communicating classes, and generate a random sequence from the Markov chain.

---
+++ MarkovChain.hpp
@@ -0,0 +1,135 @@
+/**
+ * @summary : implementation of a Markov chain. Basic class template and some functions from
+ * https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig.
+ * Note that any individual functions not written by the author here have source links in the comments above the declaration
+ * @author : Zane Jakobs
+ */
+#ifndef MarkovChain_hpp
+#define MarkovChain_hpp
+
+#include<vector>
+#include"Eigen/Core"
+#include"Eigen/Eigenvalues"
+#include<random>
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+class MarkovChain
+{
+    
+public:
+    MarkovChain();
+    
+    ~MarkovChain();
+    //transition and initial probability matrix and vector
+    
+    
+    
+    /**
+     * @name MarkovChain::setModel
+     * @summary : setModel sets parameters
+     * @param: transition: N x N transition matrix
+     * @param: initial: 1 x N initial probability row vector
+     * @source: https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig
+     */
+    
+    void setModel( Eigen::MatrixXf transition, Eigen::MatrixXf initial, int _numStates){
+        _transition = transition;
+        _initial = initial;
+        numStates = _numStates;
+    }
+    
+    void setTransition(Eigen::MatrixXf transition){
+        _transition = transition;
+}
+    
+    void setInitial(Eigen::MatrixXf initial){
+        _initial = initial;
+    }
+    void setNumStates(int _num){
+        numStates = _num;
+    }
+    
+    Eigen::MatrixXf getTransition(void) const { return _transition; }
+    Eigen::MatrixXf getInit() const { return _initial; }
+    int getNumStates(void) const {return numStates;}
+    /**
+     ** @name MarkovChain::computeProbability
+     * @summary: probability that the given sequence was generated
+     * by the Markov chain modeled by an instance of this class.
+     * @param sequence: a vector of ints, starting at 0
+     * @return: p, the probability that that the Markov chain generates
+     * this sequence.
+     */
+    float computeProbability(vector<int> sequence);
+    
+    
+    /**
+     * @name MarkovChain::randTransition
+     * @summary: initialRand: generates random state
+     * @param matrix: transition matrix
+     * @param index: current state
+     * @return index corresponding to the transition we make
+     * @source: https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig
+     * @modifications: changed random number generator to modern standard
+     */
+    int randTransition(Eigen::MatrixXf matrix, int index);
+    
+    /**
+     * @name MarkovChain::generateSequence
+     * @summary: generateSequence generates a sequence of length n from the Markov chain
+     * @param n: length of sequence
+     * @return: vector of ints representing the sequence
+     * @ source: https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig
+     */
+    vector<int> generateSequence(int n);
+    
+    /**
+     * @name MarkovChain::stationaryDistributions
+     * @summary: stationaryDistributions returns the last stationary distributions of the
+     * Markov Chain.
+     * @return: vector of floats corresponding to the last stationary distribution (by order of the eigenvalues)
+     */
+    Eigen::Matrix<complex<float>,Eigen::Dynamic,Eigen::Dynamic> stationaryDistributions();
+    
+    /**
+     * @name MarkovChain::limitingDistribution
+     * @summary: limitingDistribution computes the limiting distribution of the Markov chain
+     * @param expon: computes 10^expon powers of _transition
+     * @return limmat.row(0): limiting distribution
+     */
+    Eigen::MatrixXf limitingDistribution(int expon);
+    
+    /**
+     * @name MarkovChain::limitingMat
+     * @summary: limitingMat computes the infinite-limit matrix of the Markov chain
+     * @param expon: computes 10^expon powers of _transition
+     * @return limmat: limiting distribution matrix
+     */
+    Eigen::MatrixXf limitingMat(int expon);
+    
+    /**
+     * @author: Zane Jakobs
+     * @return: matrix of number of paths of length n from state i to state j
+     */
+    Eigen::MatrixXf numPaths(int n);
+        
+        /**
+         * @author: Zane Jakobs
+         * @return 1 or 0 for if state j can be reached from state i
+         */
+    Eigen::MatrixXf isReachable(void);
+        
+        /**
+         * @author: Zane Jakobs
+         * @return matrix where each row has a 1 in the column of each element in that communicating class (one row = one class)
+         */
+    Eigen::MatrixXf communicatingClasses(void);
+    
+private:
+    Eigen::MatrixXf _transition, _initial;
+    int numStates;
+};
+#endif
+
---
+++ markovDriver.cpp
@@ -0,0 +1,344 @@
+#include"MarkovChain.cpp"
+#include<vector>
+#include"Eigen/Core"
+#include<random>
+#include<iostream>
+using namespace std;
+/**
+ *@author Zane Jakobs
+ */
+void dispMainMenu(){
+    std::cout <<endl;
+    std::cout << "Markov Solver Main Menu: Enter Choice" <<endl;
+    std::cout << "1. Enter Transition Matrix/Initial Distribution" <<endl;
+    std::cout << "2. Enter Transition Matrix Only" <<endl;
+    std::cout << "3. Enter Initial Distribution Only" <<endl;
+    std::cout << "4. Compute Stationary Distributions" <<endl;
+    std::cout << "5. Compute Limiting Matrix" <<endl;
+    std::cout << "6. Compute Limiting Distribution" <<endl;
+    std::cout << "7. Print Matrix" <<endl;
+    std::cout << "8. Generate Sequence" <<endl;
+    std::cout << "9. Compute Number of Paths Between States" <<endl;
+    std::cout << "10. Compute Reachable States" <<endl;
+    std::cout << "11. View Communicating Classes" <<endl;
+    std::cout << "12. Quit Program" << endl;
+}
+
+int main(int argc, char* argv[]){
+    MarkovChain mc;
+    
+    bool done = false;
+    while(!done){
+        dispMainMenu();
+        string c;
+        getline(cin, c);
+        int choice;
+        try{
+            choice = stoi(c);
+        } catch(exception &msg){
+            cerr << "Error: " << msg.what() << endl;
+        }
+
+    switch(choice){
+        case 1:
+        {
+            //make matrix
+            int n;
+            string num;
+            std::cout << "For the N X N transition matrix, what is N (how many states in the chain?)?" <<endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr <<  "Error: " <<msg.what() << endl;
+                string num;
+                std::cout << "For the N X N transition matrix, what is N (how many states in the chain?)?" <<endl;
+                try{
+                    getline(cin,num);
+                    n = stoi(num);
+                } catch(exception &msg){
+                    cerr <<  "Error: " <<msg.what() << endl;
+                }
+            }
+            
+            
+            Eigen::MatrixXf p(n,n);
+            float entry;
+            string e;
+            std::cout << "Enter matrix elements one-by-one, by row, pressing enter each time." <<endl;
+            for(int i = 0; i< n; i++){
+                float rowsum = 0;
+                for(int j = 0; j<n; j++){
+                    
+                    getline(cin, e);
+                    try{
+                        entry = (float)stof(e);
+                        rowsum += entry;
+                    } catch(exception &msg){
+                        cerr << "Error: " << msg.what() << endl;
+                        j-=1;
+                    }
+                    p(i,j) = entry;
+                }//end for
+                
+                if(rowsum >= 1.01 || rowsum <= 0.99){
+                    int resp;
+                    string inresp;
+                    throw "Error: Rows do not sum to 1. Press 1 to auto-normalize, 2 to continue.";
+                    getline(cin, inresp);
+                    try{
+                        resp = stoi(inresp);
+                    } catch(exception &msg){
+                        cerr << msg.what() <<endl;
+                        std::cout << "Enter 1 or 2 only, followed by enter." <<endl;
+                    }
+                    switch(resp){
+                        case 1:
+                        {
+                            for(int j = 0; j<n; j++){
+                                p(i,j) /= rowsum;
+                            }
+                            break;
+                        }
+                        case 2:
+                        {
+                            break;
+                        }
+                    }//end switch
+                }//end if
+                
+            }//end for
+            
+            //make initial distribution
+            Eigen::MatrixXf init(1,n);
+            std::cout << "Now, enter the initial distribution. Enter " << n << " numbers,  pressing enter each time" << endl;
+            for(int i = 0; i<n; i++){
+                getline(cin, e);
+                try{
+                    entry = (float)stof(e);
+                    init(0,i) = entry;
+                } catch(exception &msg){
+                    cerr <<  "Error: " << msg.what() << endl;
+                    i-=1;
+                }
+                
+            }
+            init = init/(init.sum());
+            mc.setModel(p,init,n);
+            break;
+        }//end case 1
+            
+        case 2:
+        {
+            
+            //make matrix
+            int n;
+            string num;
+            std::cout << "For the N X N transition matrix, what is N?" <<endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr <<  "Error: " <<msg.what() << endl;
+                string num;
+                std::cout << "For the N X N transition matrix, what is N?" <<endl;
+                try{
+                    getline(cin,num);
+                    n = stoi(num);
+                } catch(exception &msg){
+                    cerr <<  "Error: " <<msg.what() << endl;
+                    break;
+                }
+            }
+            
+            Eigen::MatrixXf p(n,n);
+            float entry;
+            string e;
+            std::cout << "Enter matrix elements one-by-one, by row, pressing enter each time." <<endl;
+            for(int i = 0; i< n; i++){
+                float rowsum = 0;
+                for(int j = 0; j<n; j++){
+                    
+                    getline(cin, e);
+                    try{
+                        entry = (float)stof(e);
+                        rowsum += entry;
+                    } catch(exception &msg){
+                        cerr << "Error: " << msg.what() << endl;
+                        j-=1;
+                    }
+                    p(i,j) = entry;
+                }//end for
+                //check that rows sum to 1
+                if(rowsum >= 1.01 || rowsum <= 0.99){
+                    int resp;
+                    string inresp;
+                    std::cout<< "Error: Row does not sum to 1. Press 1 to auto-normalize, 2 to continue.";
+                    getline(cin, inresp);
+                    try{
+                        resp = stoi(inresp);
+                    } catch(exception &msg){
+                        cerr << msg.what() <<endl;
+                        std::cout << "Enter 1 or 2 only, followed by enter." <<endl;
+                    }
+                    switch(resp){
+                        case 1:
+                        {
+                            for(int j = 0; j<n; j++){
+                                p(i,j) /= rowsum;
+                            }
+                            break;
+                        }
+                        case 2:
+                        {
+                            break;
+                        }
+                    }//end switch
+                }//end if
+                
+            }//end for
+            mc.setTransition(p);
+            mc.setNumStates(n);
+            break;
+        }
+        case 3:
+        {
+            int n;
+            string num;
+            std::cout << "How many states are there?" <<endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr <<  "Error: " << msg.what() << endl;
+            }
+            //make initial distribution
+            string e;
+            float entry;
+            Eigen::MatrixXf init(1,n);
+            std::cout << "Now, enter the initial distribution. Enter " << n << " numbers,  pressing enter after each." << endl;
+            for(int i = 0; i<n; i++){
+                getline(cin, e);
+                try{
+                    entry = (float)stof(e);
+                    init(0,i) = entry;
+                } catch(exception &msg){
+                    cerr << "Error: " << msg.what() << endl;
+                    i-=1;
+                }
+                
+            }
+            try{
+                init = init/(init.sum());
+            }catch(exception &msg){
+                cerr << "Error: " << msg.what() <<endl;
+            }
+            mc.setInitial(init);
+            break;
+        }
+        
+        case 4:
+        {
+            mc.stationaryDistributions();
+            break;
+        }
+        case 5:
+        {
+            int n;
+            string num;
+            std::cout << "Calculate to how many powers of 10?" << endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr <<  "Error: " << msg.what() << endl;
+            }
+            std::cout << "The limiting matrix is " <<endl << mc.limitingMat(n) <<endl;
+            break;
+        }
+        case 6:
+        {
+            int n;
+            string num;
+            std::cout << "Calculate to how many powers of 10?" << endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr <<  "Error: " << msg.what() << endl;
+            }
+            std::cout << "The limiting distribution is ";
+            std::cout << mc.limitingDistribution(n);
+            break;
+        }
+        case 7:
+        {
+            cout << mc <<endl;
+            break;
+        }
+        case 8:
+        {
+            int n;
+            string num;
+            std::cout << "How many elements do you want in the sequence?" <<endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr << "Error: " << msg.what() << endl;
+            }
+            
+            std::cout << "Generated sequence of length " << n <<":" << endl;
+            vector<int> res(n);
+            try{
+                res = mc.generateSequence(n);
+                for(int x = 0; x < res.size(); x++){
+                    std::cout << res[x] << endl;
+                }
+            } catch(exception &msg){
+                cerr << "Error: " << msg.what() << endl;
+                cout << "You may have not set an initial distribution. Ensure one is set." << endl;
+            }
+            
+            break;
+
+        }
+        case 9:
+        {
+            int n;
+            string num;
+            std::cout << "We will compute the matrix where element (i,j) is the number of permissible paths of length ";
+            std::cout << "N from state i to state j. What is N?" <<endl;
+            getline(cin,num);
+            try{
+                n = stoi(num);
+            } catch(exception &msg){
+                cerr << "Error: " << msg.what() << endl;
+            }
+            cout << mc.numPaths(n) << endl;
+            
+        }
+        case 10:
+        {
+            int n;
+            string num;
+            std::cout << "We will compute the matrix where element (i,j) is 1 if state j can be reached from state i, 0 else.";
+            
+            cout << mc.isReachable() <<endl;
+        }
+        case 11:
+        {
+            Eigen::MatrixXf cc = mc.communicatingClasses();
+            cout << cc <<endl;
+            break;
+        }
+        case 12:
+        {
+            done = true;
+            break;
+        }
+    }//end switch
+    }//end while
+    
+}//end main
+
