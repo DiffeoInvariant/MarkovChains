abd231bf1353f81cd407a09bfe3c9dac2a1d4d70
DiffeoMan <35209763+DiffeoMan@users.noreply.github.com>
2019-02-18 19:54:34 -0700

    v0.1.2

--- MarkovChain.hpp
+++ MarkovChain.hpp
@@ -10,6 +10,7 @@
 #include<vector>
 #include"Eigen/Core"
 #include"Eigen/Eigenvalues"
+#include "MatrixFunctions.hpp"
 #include<random>
 #include<iostream>
 #include<cmath>
@@ -19,11 +20,31 @@ class MarkovChain
 {
     
 public:
-    MarkovChain();
     
-    ~MarkovChain();
-    //transition and initial probability matrix and vector
-    
+    friend std::ostream& operator<<(ostream &os, const MarkovChain &MC){
+        os << "Probability Matrix:" <<endl;
+        
+        Eigen::MatrixXd mat =  MC.getTransition();
+        os << "  || ";
+        for(int i = 0; i < mat.cols(); i++){
+            os << i << " ";
+        }
+        os <<endl << "  ";
+        
+        static const int widthMult = 6;
+        for(int i = 0; i < widthMult*mat.cols(); i++){
+            os << "=";
+        }
+        os << endl;
+        for(int i = 0; i < mat.cols(); i++){
+            os << i << " || ";
+            for(int j = 0; j < mat.cols(); j++){
+                os << mat(i,j) << " , ";
+            }
+            os << "||" <<endl;
+        }
+        return os;
+    }
     
     
     /**
@@ -54,15 +75,20 @@ public:
     Eigen::MatrixXd getTransition(void) const { return _transition; }
     Eigen::MatrixXd getInit() const { return _initial; }
     int getNumStates(void) const {return numStates;}
-    /**
-     ** @name MarkovChain::computeProbability
-     * @summary: probability that the given sequence was generated
-     * by the Markov chain modeled by an instance of this class.
-     * @param sequence: a vector of ints, starting at 0
-     * @return: p, the probability that that the Markov chain generates
-     * this sequence.
-     */
-    double computeProbability(vector<int> sequence);
+    
+    
+    
+    
+    
+    
+    MarkovChain() {};
+    
+    ~MarkovChain(){
+        _transition.resize(0,0);
+        _initial.resize(0,0);
+        numStates = 0;
+    }
+    //transition and initial probability matrix and vector
     
     
     /**
@@ -74,7 +100,24 @@ public:
      * @source: https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig
      * @modifications: changed random number generator to modern standard
      */
-    int randTransition(Eigen::MatrixXd matrix, int index);
+    int randTransition(Eigen::MatrixXd matrix, int index){
+        //set random seed
+        random_device rd;
+        //init Mersenne Twistor
+        mt19937 gen(rd());
+        // unif(0,1)
+        uniform_real_distribution<> dis(0.0,1.0);
+        double u = dis(gen);
+        double s = matrix(index,0);
+        int i = 0;
+        
+        while(u > s && (i < matrix.cols())){
+            s += matrix(index,i);
+            i += 1;
+        }
+        
+        return i;
+    }
     
     /**
      * @name MarkovChain::generateSequence
@@ -83,7 +126,23 @@ public:
      * @return: vector of ints representing the sequence
      * @ source: https://www.codeproject.com/Articles/808292/Markov-chain-implementation-in-Cplusplus-using-Eig
      */
-    vector<int> generateSequence(int n);
+    vector<int> generateSequence(int n){
+        std::vector<int> sequence(n);
+        int i, index;
+        i = 0;
+        index = 0;
+        
+        //generate initial state
+        int init = randTransition(_initial,0);
+        sequence[i] = init;
+        index = init;
+        for(i = 1; i<n; i++){
+            index = randTransition(_transition,index);
+            sequence[i] = index;
+        }
+        return sequence;
+        
+    }
     
     /**
      * @name MarkovChain::stationaryDistributions
@@ -91,7 +150,32 @@ public:
      * Markov Chain.
      * @return: vector of doubles corresponding to the last stationary distribution (by order of the eigenvalues)
      */
-    Eigen::Matrix<complex<double>,Eigen::Dynamic,Eigen::Dynamic> stationaryDistributions();
+    Eigen::Matrix<complex<double>,Eigen::Dynamic,Eigen::Dynamic> stationaryDistributions(){
+        //instantiate eigensolver
+        Eigen::EigenSolver<Eigen::MatrixXd> es;
+        //transpose transition matrix to find left eigenvectors
+        Eigen::MatrixXd pt = _transition.transpose();
+        //compute evecs, evals
+        es.compute( pt, true);
+        //store evals
+        Eigen::Matrix<complex<double>,Eigen::Dynamic,Eigen::Dynamic> evals = es.eigenvalues();
+        
+        Eigen::Matrix<complex<double>,Eigen::Dynamic,Eigen::Dynamic> evecs = es.eigenvectors();
+        Eigen::Matrix<complex<double>,1,2> dotmat;
+        dotmat(0,0) = 1;
+        dotmat(0,1) = 0;
+        int count = 0;
+        int index;
+        for(int i = 0; i< 2; i++){
+            if(abs((evals(i)*dotmat).sum()) <= 1.01 && abs((evals(i)*dotmat).sum()) >= 0.99){
+                count++;
+                index = i;
+            }
+        }
+        
+        std::cout << endl << count << " eigenvalue(s) is (are) 1, and the last stationary distribution, (stateN,0), is " << (evecs.col(index).transpose())/(evecs.col(index).sum()) <<endl;
+        return (evecs.col(index).transpose())/(evecs.col(index).sum());
+    }
     
     /**
      * @name MarkovChain::limitingDistribution
@@ -99,7 +183,11 @@ public:
      * @param expon: computes 10^expon powers of _transition
      * @return limmat.row(0): limiting distribution
      */
-    Eigen::MatrixXd limitingDistribution(int expon);
+    Eigen::MatrixXd limitingDistribution(int expon){
+        Eigen::MatrixXd limmat;
+        limmat = _transition.pow(expon);
+        return limmat.row(0);
+    }
     
     /**
      * @name MarkovChain::limitingMat
@@ -107,25 +195,131 @@ public:
      * @param expon: computes 10^expon powers of _transition
      * @return limmat: limiting distribution matrix
      */
-    Eigen::MatrixXd limitingMat(int expon);
+    Eigen::MatrixXd limitingMat(int expon){
+        Eigen::MatrixXd limmat;
+        limmat = _transition.pow(expon);
+        return limmat;
+    }
     
+    /**
+     * @summary: does mat contain key?
+     * @return: answer to above question, true or false for yes or no
+     */
+    bool contains(Eigen::MatrixXd mat, double key){
+        int n = mat.cols();
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j < n; j++){
+                if(mat(i,j) == key)  return true;
+            }
+        }
+        return false;
+    }
     /**
      * @author: Zane Jakobs
      * @return: matrix of number of paths of length n from state i to state j
      */
-    Eigen::MatrixXd numPaths(int n);
+    Eigen::MatrixXd numPaths(int n){
+        Eigen::MatrixXd logicMat(numStates,numStates);
+        for(int i = 0; i< numStates; i++){
+            for(int j = 0; j < numStates; j++){
+                if(_transition(i,j) != 0){
+                    logicMat(i,j) = 1;
+                }
+                else{
+                    logicMat(i,j) = 0;
+                }
+            }//end inner for
+        }
+        if(n > 1){
+            Eigen::MatrixXd powmat = logicMat;
+            for(int i = 0; i < n; i++){
+                powmat = powmat * logicMat;
+            }
+            return powmat;
+        }
         
-        /**
-         * @author: Zane Jakobs
-         * @return 1 or 0 for if state j can be reached from state i
-         */
-    Eigen::MatrixXd isReachable(void);
+        return logicMat;
+    }
+    
+    
+    /**
+     * @author: Zane Jakobs
+     * @return 1 or 0 for if state j can be reached from state i
+     */
+    Eigen::MatrixXd isReachable(void){
+        Eigen::MatrixXd R(numStates,numStates);
+        Eigen::MatrixXd Rcomp = Eigen::MatrixXd::Identity(numStates,numStates);
         
-        /**
-         * @author: Zane Jakobs
-         * @return matrix where each row has a 1 in the column of each element in that communicating class (one row = one class)
-         */
-    Eigen::MatrixXd communicatingClasses(void);
+        Rcomp = (Rcomp + numPaths(1));
+        Eigen::MatrixXd rpow = Rcomp;
+        for(int i = 0; i < numStates - 1; i++){
+            rpow = rpow * Rcomp;
+        }
+        for(int i = 0; i< numStates; i++){
+            for(int j = 0; j<numStates; j++){
+                if(Rcomp(i,j) > 0){
+                    R(i,j) = 1;
+                }
+                else{
+                    
+                    R(i,j) = 0;
+                }
+            }
+        }
+        return R;
+    }
+    
+    
+    bool isInVec(std::vector<int> v, int key){
+        for(std::vector<int>::iterator it = v.begin(); it != v.end(); it++){
+            if( *it == key){
+                return true;
+            }
+        }
+        return false;
+    }
+    /**
+     * @author: Zane Jakobs
+     * @return matrix where each row has a 1 in the column of each element in that communicating class (one row = one class)
+     */
+    Eigen::MatrixXd communicatingClasses(void){
+        Eigen::MatrixXd CC(numStates,numStates);
+        Eigen::MatrixXd reachable = isReachable();
+        for(int i = 0; i< numStates; i++){
+            for(int j = 0; j < numStates; j++){
+                if(reachable(i,j) == 1 && reachable(j,i) == 1){
+                    CC(i,j) = 1;
+                }
+                else{
+                    CC(i,j) = 0;
+                }
+            }//end inner for
+        }//end outer for
+        return CC;
+        
+        
+        int count = 0; //how many classes?
+        bool found;
+        Eigen::MatrixXd uniqueC(1,numStates);
+        for(int i = 0; i< numStates; i++ ){
+            found = false;
+            for(int j = i +1; j < numStates; j++){
+                if(CC.row(j) == CC.row(i)){
+                    found = true;
+                }
+            }
+            if(!found){
+                if(count > 0){
+                    uniqueC.conservativeResize(uniqueC.rows() +1, uniqueC.cols());
+                }
+                uniqueC.row(count) = CC.row(i);
+                count++;
+            }
+        }//end outer for
+        
+        return uniqueC;
+        
+    }
     
 private:
     Eigen::MatrixXd _transition, _initial;
