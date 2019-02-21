12981f737bcea9a769379180ad23f5b2a1463e98
Zane Jakobs <zane.jakobs@colorado.edu>
2019-02-21 13:06:11 -0700

    v.0.1.3
    Added stopping time calculations (expected hitting time, mean time in state before returning to X_0, and mean time in state i, starting at state j, ending at state k).

--- MarkovChain.hpp
+++ MarkovChain.hpp
@@ -11,6 +11,7 @@
 #include"Eigen/Core"
 #include"Eigen/Eigenvalues"
 #include "MatrixFunctions.hpp"
+#include"Eigen/Dense"
 #include<random>
 #include<iostream>
 #include<cmath>
@@ -63,6 +64,7 @@ public:
     
     void setTransition(Eigen::MatrixXd transition){
         _transition = transition;
+        numStates = transition.cols();
 }
     
     void setInitial(Eigen::MatrixXd initial){
@@ -317,6 +319,222 @@ public:
         return uniqueC;
         
     }
+    /**
+     * @author: Zane Jakobs
+     * @return: expected value of (T = min n >= 0 s.t. X_n  = sh) | X_0 = s0
+     * @param s0: initial state
+     * @param sh: target state
+     */
+    double expectedHittingTime(int s0, int sh){
+        /**
+         Algorithm: let u_i = E[T|X_0 = i]. Set up system of equations. Solve for u_s0. We have the equation Au = c, where u = (u_0, u_1, ..., u_n)^T, c = (-1,-1,...,0,-1,...-1)^T, with 0 in the spot corresponding to sh, and
+         
+                            [p00-1, p01, p02, ... p0n]
+                            [p10, p11-1, p12, ... p1n]
+                    A =     [p20, p21, p22-1, ... p1n]
+                                      ...
+                            [0, 0, 0, ... 1, 0, ... 0]
+                            [p(sh+1)0, ..., p(sh+1)n]
+                            [pn0-1, pn1, pn2, ... pnn-1]
+         
+         where the 1 in the sh-th row is in the sh position.
+         */
+        Eigen::MatrixXd A(numStates, numStates);
+        Eigen::VectorXd u(numStates);
+        Eigen::VectorXd c(numStates);
+        
+        for(int i = 0; i<numStates; i++){
+            //fill c
+            if(i == sh){
+                c(i) = 0;
+            }
+            else{
+                c(i) = -1;
+            }
+        //fill A
+        for(int j = 0; j< numStates; j++){
+            //if we're in the row of 0's and a 1
+            if(i == sh){
+                if(j == sh){
+                    A(i,j) = 1;
+                }
+                else{
+                    A(i,j) = 0;
+                }
+            }//end if
+            else{
+                (j == i) ? (A(i,j) = _transition(i,j)-1) : (A(i,j) == _transition(i,j));
+            }
+        }//end inner for
+    }//end outer for
+    
+    /*choose linear solver based on matrix size. Using info from Eigen docs at
+     https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html
+     */
+        if(numStates < 500){
+            //for smaller matrices, rank-revealing Householder QR decomposition with column pivoting
+            Eigen::ColPivHouseholderQR<Eigen::MatrixXd> CPH(A);
+            u = CPH.solve(c);
+
+        }
+        else{
+            //for big ones, use regular Householder QR
+            Eigen::HouseholderQR<Eigen::MatrixXd> HQR(A);
+            u = HQR.solve(c);
+        }
+        const double err_tol = 1.0e-4; //error tolerance on solutions
+        double relative_error = (A*u - c).norm() / c.norm();
+        if(relative_error < err_tol){
+            return u(s0);
+        }
+        else{
+            return -1; //return -1 if not in error bound
+        }
+    }
+    
+    /**
+     * @author: Zane Jakobs
+     * @param s0: initial state
+     * @param sInt: intermediate state
+     * @param sEnd: end state
+     * @return: mean time the chain spends in sInt, starting at s0, before returning to s0
+     */
+    double meanTimeInStateBeforeReturn(int s0, int sInt){
+        /*
+         Algorithm: We want to solve Aw = c, where c = (0,0,...,-1,0,...,0)^T, with the -1 in the sInt position, and w = (w0, w1, ..., wsInt, ... ,wn)^T. We want ws0. We also define
+         
+                [p00-1, p01, ..., 0, ...,p0n]
+                [p10, p11-1, ..., 0, ...,p1n]
+         A =    [p20, p21, ..., 0, ...,  p2n]
+                            ...
+                [p(sEnd+1)0, p(sEnd+1)1, ...]
+                [pn0, pn1, ..., 0, ..., pnn-1]
+         
+         where the 0's are in the s0 column
+         */
+        Eigen::MatrixXd A(numStates, numStates);
+        Eigen::VectorXd w(numStates);
+        Eigen::VectorXd c(numStates);
+        
+        for(int i = 0; i<numStates; i++){
+            //fill c
+            if(i == sInt){
+                c(i) = -1;
+            }
+            else{
+                c(i) = 0;
+            }
+            //fill A
+            for(int j = 0; j< numStates; j++){
+                //if we're in column s0
+                if(j == s0){
+                    (i == s0) ? A(i,j) = -1 : A(i,j) = 0;
+                }
+                else{
+                    (j == i) ? A(i,j) = _transition(i,j) -1 : A(i,j) = _transition(i,j);
+                }
+            }//end inner for
+        }//end outer for
+        
+        /*choose linear solver based on matrix size. Using info from Eigen docs at
+         https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html
+         */
+        if(numStates < 500){
+            //for smaller matrices, rank-revealing Householder QR decomposition with column pivoting
+            Eigen::ColPivHouseholderQR<Eigen::MatrixXd> CPH(A);
+            w = CPH.solve(c);
+        }
+        else{
+            //for big ones, use regular Householder QR
+            Eigen::HouseholderQR<Eigen::MatrixXd> HQR(A);
+            w = HQR.solve(c);
+        }
+        const double err_tol = 1.0e-4; //error tolerance on solutions
+        double relative_error = (A*w - c).norm() / c.norm();
+        if(relative_error < err_tol){
+            return w(s0);
+        }
+        else{
+            return -1; //return -1 if not in error bound
+        }
+    }
+    /**
+     * @author: Zane Jakobs
+     * @param s0: initial state
+     * @param sInt: intermediate state
+     * @param sEnd: end state
+     * @return: mean time the chain spends in sInt, starting at s0, before hitting sEnd
+     */
+    double meanTimeInStateBeforeHit(int s0, int sInt, int sEnd){
+        /*
+         Algorithm: We want to solve Aw = c, where c = (0,0,...,-1,0,...,0)^T, with the -1 in the sInt position, and w = (w0, w1, ..., wsInt, ... ,wn)^T. We want ws0. We also define
+         
+                [p00-1, p01, ..., 0, ...,p0n]
+                [p10, p11-1, ..., 0, ...,p1n]
+         A =    [p20, p21, ..., 0, ...,  p2n]
+                            ...
+                [0, 0, 0, ... , 0, ..., 0, 0]
+                [p(sEnd+1)0, p(sEnd+1)1, ...]
+                [pn0, pn1, ..., 0, ..., pnn-1]
+         
+         where the 0's are in the sEnd row and column
+         */
+        Eigen::MatrixXd A(numStates, numStates);
+        Eigen::VectorXd c(numStates);
+        Eigen::VectorXd w(numStates);
+        //initialize c
+        for(int i = 0; i < numStates; i++){
+            if(i == sInt){
+                c(i) = -1;
+            }
+            else{
+                c(i) = 0;
+            }
+        }//end for
+        
+        //initialize A
+        for(int i = 0; i< numStates; i++){
+            if(i == sEnd){
+                for(int j = 0; j< numStates; j++){
+                    A(i,j) = 0;
+                }
+            }//end if
+            else{
+                for(int j = 0; j< numStates; j++){
+                    (j== sEnd) ? A(i,j) = 0 : A(i,j) = _transition(i,j);
+                    if(j == i){
+                        A(i,j) -= 1.0;
+                    }
+                }
+            }//else
+        }//end outer for
+        
+        /*choose linear solver based on matrix size. Using info from Eigen docs at
+         https://eigen.tuxfamily.org/dox/group__TutorialLinearAlgebra.html
+         */
+        if(numStates < 500){
+            //for smaller matrices, rank-revealing Householder QR decomposition with column pivoting
+            Eigen::ColPivHouseholderQR<Eigen::MatrixXd> CPH(A);
+            w = CPH.solve(c);
+        }
+        else{
+            //for big ones, use regular Householder QR
+            Eigen::HouseholderQR<Eigen::MatrixXd> HQR(A);
+            w = HQR.solve(c);
+            
+        }
+        const double err_tol = 1.0e-4; //error tolerance on solutions
+        double relative_error = (A*w - c).norm() / c.norm();
+        if(relative_error < err_tol){
+            return w(s0);
+        }
+        else{
+            return -1; //return -1 if not in error bound
+        }
+        
+        
+    }//end function
+    
     
 private:
     Eigen::MatrixXd _transition, _initial;
